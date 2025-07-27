8b3d64c858084855c43c4db68498b9a62e763d25
Gaurav <gauravx999@gmail.com>
2025-07-27 19:03:49 +0530

    assets build

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
-import './index.css' // ✅ Tailwind styles
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  
     <App />
-  </React.StrictMode>
+  
 )
